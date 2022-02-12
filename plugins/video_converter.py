b87ab51b243078ffc64ba5a31f6770e526c60ce7
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-12 07:33:38 +0530

    Update video_converter.py

--- plugins/video_converter.py
+++ plugins/video_converter.py
@@ -48,9 +48,9 @@ async def convert_to_video(bot, update):
         except UserNotParticipant:
             #await update.reply_text(f"Join @{update_channel} To Use Me")
             await update.reply_text(
-                text="**Please Join My Update Channel Before Using Me..**",
+                text="**Please Join the Channel Before Using Me..**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+                    [ InlineKeyboardButton(text="Join Channel", url=f"https://t.me/{update_channel}")]
               ])
             )
             return  
